2837e437b0c1afcfd8dfe960ddb5769c2612178c
Mariana <marianamrs1995@hotmail.com>
2019-01-19 16:33:46 -0200

    first commit

---
+++ starter-code/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (request, response, next) => {
+    response.sendFile(__dirname + '/views/home.html');
+  });
+
+app.get('/about', (request, response, next) => {
+    response.sendFile(__dirname + '/views/about.html');
+});  
+
+app.get('/photo-gallery', (request, response, next) => {
+    response.sendFile(__dirname + '/views/photo-gallery.html');
+});
+
+
+app.listen(3001, () => { console.log('foi')});
